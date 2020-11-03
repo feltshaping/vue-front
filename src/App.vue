1027d16cb1b2b818e63d363ae0c42258418b832e
sunxmaz <sunxmaz@163.com>
2020-11-03 10:19:19 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <ul>
+      <li v-for="user in users" :key="user.id">{{user.name}}</li>
+    </ul>
+    <router-view/>
+  </div>
+</template>
+<script>
+import axios from 'axios'
+
+export default {
+  name:'app',
+  data(){return {users:[]}},
+  mounted(){axios.get('http://localhost:3000/api/users').then(res=>{this.users=res.data})}  
+}
+</script>
+
+<style>
+
+</style>
