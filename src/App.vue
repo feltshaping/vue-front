c1f076ee0ad7fa24cf14e56361daf7e203cd606a
sunxm <sunxmaz@163.com>
2021-06-04 10:53:47 +0800

    conf

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,25 @@
 <template>
   <div id="app">
     <ul>
-      <li v-for="user in users" :key="user.id">{{user.name}}</li>
+      <li v-for="user in users" :key="user.id">{{ user.name }}</li>
     </ul>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 <script>
-import axios from 'axios'
+import axios from 'axios';
 
 export default {
-  name:'app',
-  data(){return {users:[]}},
-  mounted(){axios.get('http://localhost:3000/api/users').then(res=>{this.users=res.data})}  
-}
+  name: 'app',
+  data() {
+    return { users: [] };
+  },
+  mounted() {
+    axios.get('http://1.116.145.253/:3000/api/users').then((res) => {
+      this.users = res.data;
+    });
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
